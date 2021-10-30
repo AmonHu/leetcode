36c2d09af9de53f2faaedaf82aa68c8532d8e54b
armon <armon.hu@outlook.com>
2021-10-30 19:59:11 +0800

    1, 翻转字符串

---
+++ string/reverse-string/reverse-string.ts
@@ -0,0 +1,10 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+  for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+    let temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+  }
+};
